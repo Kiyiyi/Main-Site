6e355a925907e08914c05b723c6e6c4dd507d246
kaijr <kaijrojo@gmail.com>
2023-01-27 09:27:15 -0500

    added tiles.js file

---
+++ tiles.js
@@ -0,0 +1,21 @@
+document.getElementById("seo").addEventListener("touchstart", touchHandler, false);
+document.getElementById("seo").addEventListener("touchend", touchHandler, false);
+document.getElementById("ui").addEventListener("touchstart", touchHandler, false);
+document.getElementById("ui").addEventListener("touchend", touchHandler, false);
+document.getElementById("accessibility").addEventListener("touchstart", touchHandler, false);
+document.getElementById("accessibility").addEventListener("touchend", touchHandler, false);
+document.getElementById("digitize").addEventListener("touchstart", touchHandler, false);
+document.getElementById("digitize").addEventListener("touchend", touchHandler, false);
+document.getElementById("trust").addEventListener("touchstart", touchHandler, false);
+document.getElementById("trust").addEventListener("touchend", touchHandler, false);
+
+function touchHandler(e)
+{
+    if (e.type == "touchstart")
+    {
+        alert("You touched the screen on " + e.type);
+    } else if (e.type == "touchend")
+    {
+        alert("You removed your finger from " + e.type);
+    }
+}
