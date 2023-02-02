ebca90ef28663cb73096a7b6e90bed3429bdbcaf
kaijr <kaijrojo@gmail.com>
2023-02-01 19:10:56 -0500

    started to create a new animation for the tagline

--- tiles.js
+++ tiles.js
@@ -1,25 +1,50 @@
-document.getElementById("seo").addEventListener("touchstart", touchHandler, false);
-document.getElementById("seo").addEventListener("touchend", touchHandler, false);
-document.getElementById("ui").addEventListener("touchstart", touchHandler, false);
-document.getElementById("ui").addEventListener("touchend", touchHandler, false);
-document.getElementById("accessibility").addEventListener("touchstart", touchHandler, false);
-document.getElementById("accessibility").addEventListener("touchend", touchHandler, false);
-document.getElementById("digitize").addEventListener("touchstart", touchHandler, false);
-document.getElementById("digitize").addEventListener("touchend", touchHandler, false);
-document.getElementById("trust").addEventListener("touchstart", touchHandler, false);
-document.getElementById("trust").addEventListener("touchend", touchHandler, false);
+var NumberOfWords = 28
 
-function touchHandler(e)
-{
-    if (e.type == "touchstart")
-    {
-        alert("You touched the screen on ");
-    } else if (e.type == "touchend")
-    {
-        alert("You removed your finger from ");
-    }
-    else
-    {
-        alert("none detected");
-    }
+var words = new BuildArray(NumberOfWords)
+
+// Fill random list
+words[1] = "Conquer the Internet"
+words[2] = "brightwork"
+words[3] = "verkrampte"
+words[4] = "protectrix"
+words[5] = "nudibranch"
+words[6] = "grandchild"
+words[7] = "newfangled"
+words[8] = "flugelhorn"
+words[9] = "mythologer"
+words[10] = "pluperfect"
+words[11] = "jellygraph"
+words[12] = "quickthorn"
+words[13] = "rottweiler"
+words[14] = "technician"
+words[15] = "cowpuncher"
+words[16] = "middlebrow"
+words[17] = "jackhammer"
+words[18] = "triphthong"
+words[19] = "wunderkind"
+words[20] = "dazzlement"
+words[21] = "jabberwock"
+words[22] = "witchcraft"
+words[23] = "pawnbroker"
+words[24] = "thumbprint"
+words[25] = "motorcycle"
+words[26] = "cryptogram"
+words[27] = "torchlight"
+words[28] = "bankruptcy"
+
+function BuildArray(size){
+this.length = size
+for (var i = 1; i <= size; i++){
+this[i] = null}
+return this
+}
+
+function PickRandomWord(frm) {
+// Generate a random number between 1 and NumberOfWords
+var rnd = Math.ceil(Math.random() * NumberOfWords)
+
+// Display the word inside the text box
+var tagline = document.getElementById("tagline");
+tagline.innerText = words[rnd]
+
 }
